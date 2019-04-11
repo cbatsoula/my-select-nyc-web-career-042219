2c7a5e94e1f504d191a73eb3eb55d144a90dcdc4
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-11 02:41:24 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -10,3 +10,14 @@ def my_select(array)
   end 
   select 
 end 
+
+def my_select(array)
+#  i = 0 
+#  nuarray = []
+#  while i < array.length 
+#    nuarray << yield(array[i])
+#    i += 1 
+#    end 
+#  array
+#end
+
